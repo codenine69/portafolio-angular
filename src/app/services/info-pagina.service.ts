c716d6967f87719fff4544791fc0229ca3c0d910
Jhon Nina <ninus1988@gmail.com>
2022-10-09 15:17:47 -0500

    fin de la seccion 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,16 +10,31 @@ export class InfoPaginaService {
   info: InfoPagina ={};
   cargada : boolean = false;
 
+  equipo: any[] = [];
+
   constructor(private http: HttpClient) { 
-    console.log('servicio de info pagina listo');
+    console.log('servicio de info pagina listo'); 
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
 
+  private cargarInfo(){
     this.http.get('assets/data/data-pagina.json')
-        .subscribe((resp: InfoPagina) =>{
-          //console.log(resp);
-          this.cargada = true;
-          this.info = resp;
-          console.log( resp );
-        });
+    .subscribe((resp: InfoPagina) =>{
+      //console.log(resp);
+      this.cargada = true;
+      this.info = resp;
+      //console.log( resp );
+    });
+  }
 
+  private cargarEquipo(){ //de firebase
+    this.http.get('https://angular-html-ed040-default-rtdb.firebaseio.com/equipo.json')
+    .subscribe((resp: any) =>{
+
+      this.equipo = resp;
+
+     //console.log( resp );
+    });
   }
 }
